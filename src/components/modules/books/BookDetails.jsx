04e4f31ad3ea241321eb9dc940d41b45eb6a0230
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-22 16:21:19 +0530

    feat: add book edit functionality in book details page

--- src/components/modules/books/BookDetails.jsx
+++ src/components/modules/books/BookDetails.jsx
@@ -18,6 +18,8 @@ import {
 import { Button } from "@heroui/react";
 import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
+import EditBookModal from "../dashboard/librarian/EditBookModal";
+import { useRouter } from "next/navigation";
 
 // Reviews Mock Data
 const mockReviews = [
@@ -40,7 +42,12 @@ const mockReviews = [
 ];
 
 export default function BookDetails({ books }) {
+  const router = useRouter();
+
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [selectedBook, setSelectedBook] = useState(null);
 
   const { data: session } = authClient.useSession();
   const loggedInUser = session?.user;
@@ -73,6 +80,22 @@ export default function BookDetails({ books }) {
       setIsSubmitting(false);
     }
   };
+
+  // book edit modal function
+  const handleEditClick = (book) => {
+    setSelectedBook(book);
+    setIsEditing(true);
+  };
+
+  if (isEditing) {
+    return (
+      <EditBookModal
+        selectedBook={selectedBook}
+        onCancel={() => setIsEditing(false)}
+        onUpdateSuccess={() => router.refresh()}
+      />
+    );
+  }
 
   return (
     <div className="max-w-7xl mx-auto p-4 md:p-6 font-urbanist text-foreground space-y-10">
@@ -210,6 +233,7 @@ export default function BookDetails({ books }) {
             {isOwnerLibrarian ? (
               <div className="grid grid-cols-3 gap-3 pt-2 w-full">
                 <Button
+                  onClick={() => handleEditClick(books)}
                   className="bg-blue-500/10 text-blue-500 border border-blue-500/20 h-12 font-bold rounded-xl text-xs uppercase font-poppins tracking-wider cursor-pointer transition-transform active:scale-95"
                   startContent={<Edit3 size={14} />}
                 >
