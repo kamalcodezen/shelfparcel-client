96c2e7564acd03be046a66306d01a36a15c45aee
kamalcodezen <kamaluddin7908@gmail.com>
2026-06-28 02:52:30 +0530

    feat: add metadata for user profile page

--- src/app/(dashboard)/dashboard/user/profile/page.jsx
+++ src/app/(dashboard)/dashboard/user/profile/page.jsx
@@ -2,6 +2,16 @@ import UserProfile from "@/components/modules/dashboard/user/UserProfile";
 import { getUserPaymentDetailsByEmail } from "@/lib/api/payment";
 import { getUserSession } from "@/lib/core/session";
 
+export const metadata = {
+  title: "My Profile | BiblioDrop",
+  description:
+    "Manage your profile, track your reading activity, and update your account information.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
 const UserProfilePage = async () => {
   const session = await getUserSession();
   const userPayment = await getUserPaymentDetailsByEmail(session?.email);
